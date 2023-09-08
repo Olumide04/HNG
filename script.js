b8c1fca85df7eec52fbe768760557bdcfb2a7794
Olumide Ayeromara <ayeromara12@gmail.com>
2023-09-08 21:16:30 +0100

    commit

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const day = document.querySelector(".day");
 const time = document.querySelector(".time");
 
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const d = new Date();
-const dayNames = days[d.getDay()];
-const milliTime = Date.now();
-day.textContent = dayNames;
-time.textContent = milliTime;
+const getUserDate = function () {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date();
+  const dayNames = days[d.getDay()];
+  const milliTime = Date.now();
+  day.textContent = dayNames;
+  time.textContent = milliTime;
+};
+getUserDate();
+setInterval(getUserDate, 1000);
